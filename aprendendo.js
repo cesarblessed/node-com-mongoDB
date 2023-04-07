5336cdc22253132006e04cf4436c148c14b514c7
cesinha1 <cesardepaula80@gmail.com>
2023-04-07 19:08:00 -0300

    estruturando os models do mongo

--- aprendendo.js
+++ aprendendo.js
@@ -9,3 +9,29 @@ mongoose.connect("mongodb://127.0.0.1:27017/aprendendo", {
 }).catch((err) => {
     console.log("Houve um erro ao se conectar: " + err);
 })
+
+// Model - Usuários
+
+const UsuarioSchema = mongoose.Schema({
+    nome: {
+        type: String,
+        require: true
+    },
+    sobrenome: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        require: true
+    },
+    idade: {
+        type: Number,
+        require: true
+    },
+    pais: {
+        type: String
+    }
+});
+
+mongoose.model('usuarios', UsuarioSchema);
